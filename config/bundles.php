33a8a02217c6501a55e67c931d9ac6ae1c26c1eb
Mahfoud Bousba <mahfoudbousba2@gmail.com>
2021-08-12 22:40:51 +0100

    Add HomeController the home page created

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
@@ -12,5 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,68 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return new Response(" I am a about page");
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
+    }
+    /**
+     * @Route("/shop", name="shop")
+     */
+    public function shop(): Response
+    {
+        return new Response(" I am a shop page");
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return new Response(" I am a contact page");
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
+    }
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function cart(): Response
+    {
+        return new Response(" I am a cart page");
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
+    }
+    /**
+     * @Route("/account", name="account")
+     */
+    public function account(): Response
+    {
+        return new Response(" I am a account page");
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
     }
 }
