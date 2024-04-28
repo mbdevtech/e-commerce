2d194f9a2b2dea2cdcdff2808a92b43c6545cfc6
Mahfoud Bousba <mahfoudbousba2@gmail.com>
2024-04-28 15:25:56 -0400

    Add some cruds to Admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Brand;
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Role;
+use App\Entity\Tax;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,12 +20,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ProductCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -36,12 +42,17 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('E Commerce');
+            ->setTitle('MyShop Admin');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('products', 'fas fa-list', Product::class);
+        yield MenuItem::linkToCrud('Products', 'fas fa-list', Product::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Roles', 'fas fa-list', Role::class);
+        yield MenuItem::linkToCrud('Taxes', 'fas fa-list', Tax::class);
+        yield MenuItem::linkToCrud('Brands', 'fa fa-list', Brand::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
     }
 }
